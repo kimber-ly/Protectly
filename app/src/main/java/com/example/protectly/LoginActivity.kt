8ed0765aa75132035bd41ca61e7de04cfb235db3
kimberly <kimberly08234@gmail.com>
2023-10-09 15:30:24 +0700

    Another commit

--- app/src/main/java/com/example/protectly/LoginActivity.kt
+++ app/src/main/java/com/example/protectly/LoginActivity.kt
@@ -1,11 +1,27 @@
 package com.example.protectly
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var btnLogin: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        btnLogin = findViewById(R.id.btn_login)
+        btnLogin.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.btn_login -> {
+                val intent = Intent(this@LoginActivity, MainActivity::class.java)
+                startActivity(intent)
+            }
+        }
     }
 }
