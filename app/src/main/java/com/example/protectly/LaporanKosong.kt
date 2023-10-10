2a44a5986d717fbead830e85fef1d73f388ef4a5
Fatimatuzzahra1 <fazzahrah125@gmail.com>
2023-10-10 18:43:19 +0700

    screen Laporan kosong

--- app/src/main/java/com/example/protectly/LaporanKosong.kt
+++ app/src/main/java/com/example/protectly/LaporanKosong.kt
@@ -1,11 +1,29 @@
 package com.example.protectly
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
-class LaporanKosong : AppCompatActivity() {
+class LaporanKosong : AppCompatActivity(), View.OnClickListener {
+    private lateinit var btnkirim: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_laporan_kosong)
+
+        btnkirim = findViewById(R.id.btn_kirim)
+        btnkirim.setOnClickListener(this)
+}
+
+    override fun onClick(v: View?){
+        when(v?.id){
+            R.id.btn_kirim -> {
+                val intent = Intent(this@LaporanKosong, StatusLaporanActivity::class.java)
+                startActivity(intent)
+            }
+        }
+
     }
 }
