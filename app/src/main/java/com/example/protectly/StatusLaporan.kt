515b1b113dafb85d03d849866258fea615990083
Fatimatuzzahra1 <fazzahrah125@gmail.com>
2023-10-11 16:12:42 +0700

    Update 11 oktober

---
+++ app/src/main/java/com/example/protectly/StatusLaporan.kt
@@ -0,0 +1,3 @@
+package com.example.protectly
+
+data class StatusLaporan (val imgView: Int, val txtStatus: String, val txtJenis: String, val txtWaktu: String, val txtKode: String)
--- app/src/main/java/com/example/protectly/StatusLaporanActivity.kt
+++ app/src/main/java/com/example/protectly/StatusLaporanActivity.kt
@@ -2,10 +2,24 @@ package com.example.protectly
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 
 class StatusLaporanActivity : AppCompatActivity() {
+
+    lateinit var recyclerview: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_status_laporan)
+
+        init()
+    }
+
+    private fun init(){
+        recyclerview = findViewById(R.id.recycler_view)
+
+        var data = ArrayList<StatusLaporan>()
+        data.add(StatusLaporan(R.drawable.baseline_arrow_forward, "Penyidikan", "Penipuan""Dilaporkan 22 September 2022", "Kode: XXXX/XXXXX/XXXXX"))
+        data.add(StatusLaporan(R.drawable.baseline_arrow_forward, "Penyelidikan", "Pembegalan""Dilaporkan 22 September 2022", "Kode: XXXX/XXXXX/XXXXX"))
     }
 }
