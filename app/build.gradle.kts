c2b6f5667eeb4f67ec68ccb140b7501a32a733d8
kimberly <kimberly08234@gmail.com>
2023-10-11 16:32:09 +0700

    Commit 11 Okt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,16 +41,16 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation ("com.google.android.gms:play-services-maps:17.0.1")
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation ("com.google.android.gms:play-services-maps:18.1.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("com.google.android.gms:play-services-maps:18.1.0")
     implementation("com.android.support:support-annotations:28.0.0")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
-    implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.4")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.4")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
--- app/src/main/java/com/example/protectly/LoginActivity.kt
+++ app/src/main/java/com/example/protectly/LoginActivity.kt
@@ -1,11 +1,10 @@
 package com.example.protectly
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import com.example.protectly.ui.home.HomeFragment
+import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var btnLogin: Button
@@ -20,7 +19,7 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
     override fun onClick(v: View?) {
         when(v?.id){
             R.id.btn_login -> {
-                val intent = Intent(this@LoginActivity, ProfileActivity::class.java)
+                val intent = Intent(this@LoginActivity, NavBarActivity::class.java)
                 startActivity(intent)
             }
         }
