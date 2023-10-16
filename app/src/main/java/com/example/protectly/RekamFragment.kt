77702393d618cee9a7fe6b174a7e0f1318cf8c68
kimberly <kimberly08234@gmail.com>
2023-10-16 21:08:41 +0700

    Commit 16 Okt fitur kamera

--- app/src/main/java/com/example/protectly/RekamFragment.kt
+++ app/src/main/java/com/example/protectly/RekamFragment.kt
@@ -1,59 +1,99 @@
 package com.example.protectly
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [RekamFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class RekamFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var rekamImg: ImageView
+    private lateinit var captureButton: ImageView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_rekam, container, false)
+        val view = inflater.inflate(R.layout.fragment_rekam, container, false)
+        rekamImg = view.findViewById(R.id.rekam_img)
+        captureButton = view.findViewById(R.id.btn_capture)
+
+        captureButton.setOnClickListener {
+            if (ContextCompat.checkSelfPermission(
+                    requireContext(), Manifest.permission.CAMERA
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                startCameraCapture()
+            } else {
+                ActivityCompat.requestPermissions(
+                    requireActivity(), arrayOf(Manifest.permission.CAMERA), CAMERA_PERMISSION_CODE
+                )
+            }
+        }
+
+        return view
+    }
+
+    private fun startCameraCapture() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
+        if (intent.resolveActivity(requireContext().packageManager) != null){
+            intent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 1)
+            intent.putExtra(MediaStore.EXTRA_SIZE_LIMIT, 1024 * 1024)
+
+            startActivityForResult(intent, CAMERA_REQUEST_CODE)
+        }
+        else {
+            Toast.makeText(requireContext(), "Camera app not found.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == CAMERA_PERMISSION_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startCameraCapture()
+            } else {
+                Toast.makeText(
+                    requireContext(), "Camera permission denied", Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == CAMERA_REQUEST_CODE) {
+                val thumbnail: Bitmap? = data?.extras?.get("data") as Bitmap?
+                thumbnail?.let {
+                    rekamImg.setImageBitmap(it)
+                }
+            }
+        }
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RekamFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            RekamFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        private const val CAMERA_PERMISSION_CODE = 1
+        private const val CAMERA_REQUEST_CODE = 2
     }
+
 }
