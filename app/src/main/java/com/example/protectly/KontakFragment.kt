53b0a233285d150e3124aa74c90d1cc5fda0b7d8
kimberly <kimberly08234@gmail.com>
2023-10-17 16:18:30 +0700

    Commit 17 Okt fitur kamera

--- app/src/main/java/com/example/protectly/KontakFragment.kt
+++ app/src/main/java/com/example/protectly/KontakFragment.kt
@@ -1,23 +1,30 @@
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.viewpager.widget.ViewPager
 import com.example.protectly.PagerAdapterKontak
+import com.example.protectly.PesanDaruratActivity
 import com.example.protectly.R
 import com.google.android.material.tabs.TabLayout
 
-class KontakFragment : Fragment() {
+class KontakFragment : Fragment(), View.OnClickListener {
     private lateinit var tab: TabLayout
     private lateinit var viewPager: ViewPager
+    private lateinit var btnSos: ImageView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_kontak, container, false)
+        btnSos = view.findViewById(R.id.btn_sos)
+        btnSos.setOnClickListener(this)
+
         viewPager = view.findViewById(R.id.viewPager)
 
         val adapter = PagerAdapterKontak(childFragmentManager)
@@ -28,6 +35,15 @@ class KontakFragment : Fragment() {
         tab.setupWithViewPager(viewPager)
 
         return view
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id){
+            R.id.btn_sos -> {
+                val intent = Intent(requireContext(), PesanDaruratActivity::class.java)
+                startActivity(intent)
+            }
+        }
     }
 
 }
