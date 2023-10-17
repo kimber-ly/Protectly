329894fe23e42a3002a3d36b152bd9e19c278969
Fatimatuzzahra1 <fazzahrah125@gmail.com>
2023-10-17 14:34:34 +0700

    PesanDarurat

---
+++ app/src/main/java/com/example/protectly/PesanHasilRecordActivity.kt
@@ -0,0 +1,11 @@
+package com.example.protectly
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PesanHasilRecordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pesan_hasil_record)
+    }
+}
