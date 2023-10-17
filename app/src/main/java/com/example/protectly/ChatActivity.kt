50805f85c0625c7badcd4355d1f6e3dffca643a2
kimberly <kimberly08234@gmail.com>
2023-10-17 19:57:34 +0700

    Commit 17 Okt Chat

--- app/src/main/java/com/example/protectly/ChatActivity.kt
+++ app/src/main/java/com/example/protectly/ChatActivity.kt
@@ -7,15 +7,18 @@ import androidx.appcompat.app.AppCompatActivity
 
 class ChatActivity : AppCompatActivity() {
     private lateinit var profilKontak: ImageView
+    private lateinit var profilKontak2: ImageView
     private lateinit var namaKontak: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
 
         profilKontak = findViewById(R.id.profil_kontak)
+        profilKontak2 = findViewById(R.id.profil_kontak_2)
         namaKontak = findViewById(R.id.nama_kontak)
 
         profilKontak.setImageResource(intent.getIntExtra("gambar", 0))
+        profilKontak2.setImageResource(intent.getIntExtra("gambar", 0))
         namaKontak.text = intent.getStringExtra("nama")
     }
 }
